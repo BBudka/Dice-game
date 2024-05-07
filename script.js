3fa10c64c164183cd6a256f9b959b0ce31b724c3
Budjav <Budka0322@gmail.com>
2024-05-07 20:42:07 +0800

    toglogchiin eeljiig colidog bolno

--- script.js
+++ script.js
@@ -1,14 +1,9 @@
-// mdn javascript events
-// тухайн кодийг  copy paste хийж байвал  тэр кодийг сайжруулах боломжтой гэсэн үг
 // Тоглогчийн хадгалах хувьсагч
 var activePlayer = 0;
 // Тоглогчдийн цуглуулсан оноог хадгалах хувьсагч
 var scores = [0, 0];
 // Тоглогчийн ээлжиндээ цуглуулж байгаа оноог хадгалах хувьсагч
 var currentScore = 0;
-// Шооны аль талаараа буусныг хадгалах хувьсагч хэрэгтэй,
-//  1-6 гэсэн утгыг энэ хувьсагчид санамсаргүйгээр үүсгэж өгнө.
-// var dice = Math.ceil(Math.random() * 6);
 
 // id gaar elementiig ni oloh
 document.getElementById("");
@@ -28,13 +23,40 @@ DiceDom.style.display = "none";
 // uur gazart ashiglahgui uchir huivsagchid hadgalh shaardlagui
 document
   .querySelector(".btn--roll")
+  //   шоог шидэх эвент листенер
   .addEventListener("click", function shidnuu() {
     var dice = Math.floor(Math.random() * 6) + 1;
     // alert("шоо шидлээ: " + dice);
 
     // acar tom modnoos haij alj baigaa(window.document)
+    // Шооны зургийг веб дээр гарга ирнэ.
     DiceDom.style.display = "block";
+
+    // Буусан санамсаргүй тоонд харгалзах  шооны зургийг веб дээр гаргаж ирнэ
     DiceDom.src = "images/dice-" + dice + ".png";
+
+    // буусан тоо нь нэгээс ялгаатай байх юм бол идэвхтэй тоглогчийн ээлжийн огоог нэмэгдүүлнэ.
+    if (dice !== 1) {
+      // 1-ээс ялгаатай тоо буулаа
+      currentScore = currentScore + dice;
+      document.getElementById("current--" + activePlayer).textContent =
+        currentScore;
+    } else {
+      // 1 буусан тул тоглогчийн ээлжийн хэсгийг энэ хэсэгт сольж өгнө.
+      //  энэ тоглогчийн ээлжиндээ цуглуулсан оноог 0 болгох
+      currentScore = 0;
+      document.getElementById("current--" + activePlayer).textContent = 0;
+      activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+
+      //   document.querySelector(".player--0").classList.remove("player--active");
+      //   document.querySelector(".player--1").classList.add("player--active");
+
+      //байх юм бол устгадаг байхгүй бол нэмдэг - toggle
+      document.querySelector(".player--0").classList.toggle("player--active");
+      document.querySelector(".player--1").classList.toggle("player--active");
+
+      DiceDom.style.display = "none";
+    }
     console.log("Шидлээ: " + dice);
   });
 
