5f8d3c43affa8ed4a5f6621da3d402be457e954f
Budjav <Budka0322@gmail.com>
2024-05-07 18:57:34 +0800

    shoog shiddeg bolgoh

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// mdn javascript events
+// тухайн кодийг  copy paste хийж байвал  тэр кодийг сайжруулах боломжтой гэсэн үг
 // Тоглогчийн хадгалах хувьсагч
 var activePlayer = 0;
 // Тоглогчдийн цуглуулсан оноог хадгалах хувьсагч
@@ -7,16 +9,36 @@ var currentScore = 0;
 // Шооны аль талаараа буусныг хадгалах хувьсагч хэрэгтэй,
 //  1-6 гэсэн утгыг энэ хувьсагчид санамсаргүйгээр үүсгэж өгнө.
 // var dice = Math.ceil(Math.random() * 6);
-var dice = Math.floor(Math.random() * 6) + 1;
 
+// id gaar elementiig ni oloh
+document.getElementById("");
+// queryselector  ni domoos ynzburiin select nukhtsuliig huleej avch tootsoolol hiideg
 
-document.querySelector("#score--0").textContent = dice;
-document.querySelector("#score--1").textContent = dice;
-document.querySelector(".dice").style.display = "none";
+// document.getElementById("#score--0").textContent = 0;
+// id geer haij baiga bol # tavih shaardlaga baihgui
+document.querySelector("#score--0").textContent = 0;
+document.querySelector("#score--1").textContent = 0;
+
 document.querySelector("#current--1").textContent = 0;
 document.querySelector("#current--0").textContent = 0;
+var DiceDom = document.querySelector(".dice");
+DiceDom.style.display = "none";
+
+// ямар эвент дээр хариу үйлдэл үзүүлэхгээд байгаа юм бэ?
+// uur gazart ashiglahgui uchir huivsagchid hadgalh shaardlagui
+document
+  .querySelector(".btn--roll")
+  .addEventListener("click", function shidnuu() {
+    var dice = Math.floor(Math.random() * 6) + 1;
+    // alert("шоо шидлээ: " + dice);
+
+    // acar tom modnoos haij alj baigaa(window.document)
+    DiceDom.style.display = "block";
+    DiceDom.src = "images/dice-" + dice + ".png";
+    console.log("Шидлээ: " + dice);
+  });
 
 // document.querySelector("#score--1").innerHTML = "<em>YES</em>";
 // document.querySelector("#score--1").innerHTML = "YES";
-console.log("Шоо: " + dice);
-console.log("hiiiiiiiiiii")
+
+console.log("hiiiiiiiiiii");
